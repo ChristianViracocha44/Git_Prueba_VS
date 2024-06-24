d65d067e44a2b2f25ed83df982b4eb321fa74943
ChristianViracocha44 <sistemas.ti4@sedemi.com>
2024-06-24 12:56:49 -0500

    Actualizacion Chris MP
    Actualizacion del titulo Chris MP

--- PruebasGitVS/Controllers/WeatherForecastController.cs
+++ PruebasGitVS/Controllers/WeatherForecastController.cs
@@ -18,7 +18,7 @@ namespace PruebasGitVS.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
+        [HttpGet(Name = "GetWeatherForecast55")]
         public IEnumerable<WeatherForecast> Get()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
